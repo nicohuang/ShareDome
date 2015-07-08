bf90bcfa80d0a18a72c59e633261e651bc7202ac
nico <huangwenzeng1@163.com>
2015-07-08 18:44:43 +0800

    小米推送

---
+++ app/src/main/java/hwz/com/sharedome/mi/MyMessageReceiver.java
@@ -0,0 +1,17 @@
+package hwz.com.sharedome.mi;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by jan on 15/7/8.
+ */
+public class MyMessageReceiver extends BroadcastReceiver
+{
+    @Override
+    public void onReceive(Context context, Intent intent)
+    {
+
+    }
+}
